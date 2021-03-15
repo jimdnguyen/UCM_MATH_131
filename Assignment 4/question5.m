bfb50b601298ec385cf1c9e59626753aad3065c9
DaoistDuck <dragonoath123@gmail.com>
2021-03-15 14:10:11 -0700

    hi
    hi

--- Assignment 4/question5.m
+++ Assignment 4/question5.m
@@ -16,25 +16,34 @@
 f = @(x) sin(x);
 dddf = @(x) -cos(x); % the third derivative of sin(x) is -cos(x)
 x = 1; % the pt we are estimating the derivative of the function
-E = -pi; %-cos(x) is largest when x = -pi so that -cos(x) = 1
 h = [10^-1,10^-2,10^-3,10^-4,10^-5,10^-6,10^-7,10^-8,10^-9,10^-10,10^-11,10^-12,10^-13,10^-14,10^-15,10^-16]; % the multiple values of h
 CD3 = zeros(1,16);
+CD3_truncation_error = zeros(1,16);
 CD3_relative_error = zeros(1,16);
-%CD3_relative_error = h^2/6 * f^3 (E)
 
 for i = 1:16
     CD3(i) = (1/(2*h(i)) * (f(x + h(i)) - f(x - h(i))));
-    CD3_relative_error(i) = h(i)^2 / 6 * dddf(E);
+    
+    maxdddf = max(dddf(h(i))); % finds ths max value of all the elements
+    indexOfFirstMax = find(dddf(h) == maxdddf,1,'first'); % gets the first element that is the max value
+    E = h(indexOfFirstMax); % gets the x value at the index
+    
+    CD3_truncation_error(i) = -h(i)^2 / 6 * dddf(E);
+    CD3_relative_error(i) = abs(dddf(x) - CD3(i)) / abs((dddf(x)));
 end
 
-loglog(h,CD3_relative_error,'b'); % this is plotting the step size h verus relative error in a loglog plot
+loglog(h,CD3_relative_error,'y'); % this is plotting the step size h verus relative error in a loglog plot
 hold on
-loglog(CD3_relative_error,'r'); % this is just plotting the estimated truncation error for same h in red.
+loglog(h,CD3_truncation_error,'r'); % this is just plotting the estimated truncation error for same h in red.
 hold off
 grid on
 
 %%
 % *Part b*
 % We can see that as h grows smaller, then the error decreases. This can be
-% seen in the blue line. The peak of the line is when h is largest and then
-% it decreases a lot. 
+% seen in the red line. The yellow line at the very top of the graph
+% shows the truncation error with this formula. 
+% As you can see, it looks really good since the truncation error
+% isn't moving around and in general a horizontal line. The actual error
+% does follow the truncation error estimate I would assume since the
+% truncation error isn't moving around and is just mainly a straight line.
